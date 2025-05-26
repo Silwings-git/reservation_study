02a35babbff313a5191b6269d04ae5cbaf793058
Silwings <1910506332@qq.com>
2025-05-26 18:03:31 +0800

    fix DROP TABLE rsvp.server_read_cursor CASCADE;

--- migrations/20250526090311_reservation_trigger.down.sql
+++ migrations/20250526090311_reservation_trigger.down.sql
@@ -1,3 +1,4 @@
 DROP TRIGGER reservations_trigger ON rsvp.reservations;
 DROP FUNCTION rsvp.reservations_trigger();
 DROP TABLE rsvp.reservation_changes CASCADE;
+DROP TABLE rsvp.server_read_cursor CASCADE;

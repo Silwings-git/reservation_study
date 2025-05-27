1dd0da38748c546a3ce191bc6d6706dbfdf51fe4
Silwings <1910506332@qq.com>
2025-05-28 00:50:56 +0800

    6.实现预定功能

--- abi/build.rs
+++ abi/build.rs
@@ -3,10 +3,11 @@ use std::process::Command;
 fn main() {
     tonic_build::configure()
         .out_dir("src/pb")
+        .type_attribute("reservation.ReservationStatus", "#[derive(sqlx::Type)]")
         .compile_protos(&["./protos/reservation.proto"], &["protos"])
         .unwrap();
 
-    Command::new("cargo").args(&["fmt"]).output().unwrap();
+    Command::new("cargo").args(["fmt"]).output().unwrap();
 
     println!("cargo:rerun-if-changed=protos/reservation.proto");
     println!("cargo:rerun-if-changed=build.rs");
---
+++ reservation/src/manager.rs
@@ -0,0 +1,66 @@
+
+use abi::ReservationQuery;
+use async_trait::async_trait;
+use chrono::{DateTime, Utc};
+use sqlx::postgres::types::PgRange;
+use sqlx::Row;
+use crate::{ReservationId, ReservationManager, Rsvp, error::ReservationError};
+
+#[async_trait]
+impl Rsvp for ReservationManager {
+    async fn reserve(
+        &self,
+        mut rsvp: abi::Reservation,
+    ) -> Result<abi::Reservation, ReservationError> {
+        // generate a insert sql for the reservation
+        if rsvp.start.is_none() || rsvp.end.is_none() {
+            return Err(ReservationError::InvalidTime);
+        }
+
+        let start = abi::convert_to_utc_time(rsvp.start.unwrap());
+        let end = abi::convert_to_utc_time(rsvp.end.unwrap());
+        let timespan: PgRange<DateTime<Utc>> = (start..end).into();
+
+        let id = sqlx::query(
+            "INSERT INTO rsvp.reservations(user_id,resource_id,timespan,note,status) VALUES ($1,$2,$3,$4,$5) RETURNING id",
+        )
+        .bind( rsvp.user_id.clone())
+        .bind( rsvp.resource_id.clone())
+        .bind( timespan)
+        .bind( rsvp.note.clone())
+        .bind( rsvp.status)
+        .fetch_one(&self.pool)
+        .await?.get(0);
+
+        rsvp.id = id;
+
+        Ok(rsvp)
+    }
+
+    async fn change_status(&self, _id: ReservationId) -> Result<abi::Reservation, ReservationError> {
+        todo!()
+    }
+
+    async fn update_note(
+        &self,
+        _id: ReservationId,
+        _note: String,
+    ) -> Result<abi::Reservation, ReservationError> {
+        todo!()
+    }
+
+    async fn delete(&self, _id: ReservationId) -> Result<(), ReservationError> {
+        todo!()
+    }
+
+    async fn get(&self, _id: ReservationId) -> Result<abi::Reservation, ReservationError> {
+        todo!()
+    }
+
+    async fn query(
+        &self,
+        _query: ReservationQuery,
+    ) -> Result<Vec<abi::Reservation>, ReservationError> {
+        todo!()
+    }
+}
